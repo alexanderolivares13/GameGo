f75c1050e42f83925f2b534ac28be9c57115e36b
artorrias <sam.henson1119@outlook.com>
2023-08-08 20:15:32 -0600

    added files and handlebars

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,70 @@
+//import user from models
+//const router = require('express').Router();
+
+//----------------------------------------------- CREATE NEW USER
+/*
+router.post('/', async (req, res) => {
+    try {
+        const userDB = await User.create({
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password,
+        });
+
+        req.session.save(() => {
+            req.session.logged_in = true;
+
+            res.status(200).json(userDB);
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+// ----------------------------------------------- LOGIN
+
+router.post('/login', async (req, res) => {
+    try {
+        const userDB = await User.findOne({
+            where: {
+                email: req.body.email,
+            },
+        });
+
+        if (!userDB) {
+            res.status(400).json({message: 'Incorrect email or password!'});
+            return;
+        }
+
+        const validPass = await userDB.checkpassword(req.body.password); //checkpassword is a function in the user model
+
+        if (!validPass) {
+            res.status(400).json({message: 'Incorrect email or password!'});
+            return;
+        }
+
+        req.session.save(() => {
+            req.session.logged_in = true;
+
+            res.status(200).json({ user: userDB, message: 'You are now logged in'});
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+// ------------------------------------------------------------------ LOGOUT
+router.post('/logout', (req, res) => {
+  if (req.session.logged_in) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
+
+module.exports = router;
+*/
+
