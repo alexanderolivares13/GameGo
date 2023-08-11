7aa0297b0cf1bd467e6c7c06f16ce41fb13cf7fd
frvnci5 <frvncis@outlook.com>
2023-08-10 17:56:21 -0700

    code in seed.js

--- seeds/seed.js
+++ seeds/seed.js
@@ -2,6 +2,22 @@ const seedGenres = require('./genre-seeds');
 const seedVideoGame = require('./video-game-seeds');
 const seedGameGenres = require('./game-genre-seeds');
 
+const sequelize = require('../config/connection');
 
-
+const seedAll = async () => {
+    await sequelize.sync({ force: true });
+    console.log('\n----- DATABASE SYNCED -----\n');
+    await seedGenres();
+    console.log('\n----- GENRES SEEDED -----\n');
+  
+    await seedVideoGame();
+    console.log('\n----- VIDEO GAMES SEEDED -----\n');
+  
+    await seedGameGenres();
+    console.log('\n----- GAME GENRES SEEDED -----\n');
+  
+    process.exit(0);
+  };
+  
+  seedAll();
 
