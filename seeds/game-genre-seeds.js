f752462161671145c0be9c6e58f9c26f57d04961
frvnci5 <frvncis@outlook.com>
2023-08-10 18:02:59 -0700

    commented out game-genre.js

--- seeds/game-genre-seeds.js
+++ seeds/game-genre-seeds.js
@@ -1,88 +1,88 @@
-const { GameGenres } = require('../models');
+// const { GameGenres } = require('../models');
 
-const gameGenreData = [
-  {
-    genre_id: 1,
-    videogame_id: 1,
-  },
-  {
-    genre_id: 1,
-    videogame_id: 2,
-  },
-  {
-    genre_id: 1,
-    videogame_id: 3,
-  },
-  {
-    genre_id: 1,
-    videogame_id: 4,
-  },
-  {
-    genre_id: 1,
-    videogame_id: 5,
-  },
-  {
-    genre_id: 2,
-    videogame_id: 6,
-  },
-  {
-    genre_id: 2,
-    videogame_id: 7,
-  },
-  {
-    genre_id: 2,
-    videogame_id: 8,
-  },
-  {
-    genre_id: 2,
-    videogame_id: 9,
-  },
-  {
-    genre_id: 2,
-    videogame_id: 10,
-  },
-  {
-    genre_id: 3,
-    videogame_id: 11,
-  },
-  {
-    genre_id: 3,
-    videogame_id: 12,
-  },
-  {
-    genre_id: 3,
-    videogame_id: 13,
-  },
-  {
-    genre_id: 3,
-    videogame_id: 14,
-  },
-  {
-    genre_id: 3,
-    videogame_id: 15,
-  },
-  {
-    genre_id: 4,
-    videogame_id: 16,
-  },
-  {
-    genre_id: 4,
-    videogame_id: 17,
-  },
-  {
-    genre_id: 4,
-    videogame_id: 18,
-  },
-  {
-    genre_id: 4,
-    videogame_id: 19,
-  },
-  {
-    genre_id: 4,
-    videogame_id: 20,
-  },
-];
+// const gameGenreData = [
+//   {
+//     genre_id: 1,
+//     videogame_id: 1,
+//   },
+//   {
+//     genre_id: 1,
+//     videogame_id: 2,
+//   },
+//   {
+//     genre_id: 1,
+//     videogame_id: 3,
+//   },
+//   {
+//     genre_id: 1,
+//     videogame_id: 4,
+//   },
+//   {
+//     genre_id: 1,
+//     videogame_id: 5,
+//   },
+//   {
+//     genre_id: 2,
+//     videogame_id: 6,
+//   },
+//   {
+//     genre_id: 2,
+//     videogame_id: 7,
+//   },
+//   {
+//     genre_id: 2,
+//     videogame_id: 8,
+//   },
+//   {
+//     genre_id: 2,
+//     videogame_id: 9,
+//   },
+//   {
+//     genre_id: 2,
+//     videogame_id: 10,
+//   },
+//   {
+//     genre_id: 3,
+//     videogame_id: 11,
+//   },
+//   {
+//     genre_id: 3,
+//     videogame_id: 12,
+//   },
+//   {
+//     genre_id: 3,
+//     videogame_id: 13,
+//   },
+//   {
+//     genre_id: 3,
+//     videogame_id: 14,
+//   },
+//   {
+//     genre_id: 3,
+//     videogame_id: 15,
+//   },
+//   {
+//     genre_id: 4,
+//     videogame_id: 16,
+//   },
+//   {
+//     genre_id: 4,
+//     videogame_id: 17,
+//   },
+//   {
+//     genre_id: 4,
+//     videogame_id: 18,
+//   },
+//   {
+//     genre_id: 4,
+//     videogame_id: 19,
+//   },
+//   {
+//     genre_id: 4,
+//     videogame_id: 20,
+//   },
+// ];
 
-const seedGameGenres = () => GameGenres.bulkCreate(gameGenreData);
+// const seedGameGenres = () => GameGenres.bulkCreate(gameGenreData);
 
-module.exports = seedGameGenres;
+// module.exports = seedGameGenres;
