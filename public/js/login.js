753a41308cd5b9bd0f33277a21f7282e3a1e4bf5
artorrias <sam.henson1119@outlook.com>
2023-08-14 18:20:28 -0600

    changed svg to fit and testing login

--- public/js/login.js
+++ public/js/login.js
@@ -1,13 +1,14 @@
 const loginHandler = async (event) => {
     event.preventDefault();
 
-    const email = document.getElementById('email-login').value.trim();
-    const password = document.getElementById('password-login').value.trim();
+    const email = document.querySelector('email-login').value.trim();
+    const password = document.querySelector('password-login').value.trim();
 
     if (email && password) {
         const response = await fetch('/api/users/login', {
             method: 'POST',
             body: JSON.stringify({ email, password }),
+            headers: { 'Content-Type': 'application/json' },
         });
 
         if (response.ok) {
@@ -22,13 +23,14 @@ const loginHandler = async (event) => {
 const signUpHandler = async (event) => {
     event.preventDefault();
 
-    const email = document.getElementById('email-signup').value.trim();
-    const password = document.getElementById('password-signup').value.trim();
+    const email = document.querySelector('email-signup').value.trim();
+    const password = document.querySelector('password-signup').value.trim();
 
     if (email && password) {
         const response = await fetch('/api/users', {
             method: 'POST',
             body: JSON.stringify({ email, password }),
+            headers: { 'Content-Type': 'application/json' },
         });
         if (response.ok) {
             document.location.replace('/');
@@ -39,5 +41,5 @@ const signUpHandler = async (event) => {
     }
 };
 
-document.querySelector('login-form').addEventListener('submit', loginHandler);
-document.querySelector('signup-form').addEventListener('submit', signUpHandler);
+document.querySelector('.login-form').addEventListener('submit', loginHandler);
+document.querySelector('.signup-form').addEventListener('submit', signUpHandler);
