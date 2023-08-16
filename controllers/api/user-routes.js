7218fb7b5527ecb9a8d9a9519ce67c88fc0c5476
alexanderolivares13 <124226984+alexanderolivares13@users.noreply.github.com>
2023-08-15 17:22:08 -0700

    added an endpoint to the home route to handle viewing an individual game, fixed a bug in the user-route.js file, and cleaned up the main handlebars layout a little more.

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -3,7 +3,6 @@ const User = require("../../models/User");
 
 //----------------------------------------------- CREATE NEW USER
 
-
 router.post("/", async (req, res) => {
   try {
     const userDB = await User.create({
@@ -39,8 +38,7 @@ router.post("/login", async (req, res) => {
       return;
     }
 
-    const validPass = await userData.checkpassword(req.body.password); //checkpassword is a function in the user model
-
+    const validPass = await userData.checkPass(req.body.password); //checkpassword is a function in the user model
 
     if (!validPass) {
       res.status(400).json({ message: "Incorrect email or password!" });
