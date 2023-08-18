800332c99f6a171ba8fd9ade21f2c6014e842d53
alexanderolivares13 <124226984+alexanderolivares13@users.noreply.github.com>
2023-08-17 17:18:16 -0700

    added the cart npm package and a button on the main home page

---
+++ public/js/script.js
@@ -0,0 +1,10 @@
+// const cartBtn = document.querySelector('#shopping-cart');
+
+// const EcomCart = require('@ecomplus/shopping-cart');
+
+
+// const store = 3001;
+// const shoppingList = {};
+
+// const ecomCart = new EcomCart(store, 'usercart', shoppingList);
+
